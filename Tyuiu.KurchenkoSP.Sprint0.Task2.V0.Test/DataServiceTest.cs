270e5fdc01c13de815ed87127a4f3cecc69d5507
Saveliy Kurchenko <Tyuiu.KurchenkoSP@outlook.com>
2026-09-16 20:53:11 +0500

    Task2

---
+++ Tyuiu.KurchenkoSP.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KurchenkoSP.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.KurchenkoSP.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Савелий";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual($"Привет... , Савелий", res);
+        }
+    }
+}
---
+++ Tyuiu.KurchenkoSP.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,4 @@
+﻿using Tyuiu.KurchenkoSP.Sprint0.Task2.V0.Lib
+Console.WriteLine(DataService.GetMessage("Савелий"));
+Console.ReadKey();
+Console.ReadKey();
