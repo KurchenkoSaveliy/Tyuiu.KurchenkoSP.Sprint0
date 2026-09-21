a549454f31fe4999cf9e2116c4b3fb41cfae9a17
Saveliy Kurchenko <Tyuiu.KurchenkoSP@outlook.com>
2026-09-21 20:47:56 +0500

    Task4

---
+++ Tyuiu.KurchenkoSP.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using
+    Tyuiu.KurchenkoSP.Sprint0.Task4.V0.Lib;
+
+Console.WriteLine (DataService.Addition(5, 10));
+
+Console.WriteLine(DataService.Subtraction(10, 5));
+
+Console.WriteLine(DataService.Multiplication(10, 5));
+
+Console.WriteLine(DataService.Division(10, 5));
+
+Console.ReadKey ();
