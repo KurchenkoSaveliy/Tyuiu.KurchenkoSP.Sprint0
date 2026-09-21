58694ccc8d5ded53ad5e169319dcdbc23254542a
Saveliy Kurchenko <Tyuiu.KurchenkoSP@outlook.com>
2026-09-21 21:07:42 +0500

    Task5

---
+++ Tyuiu.KurchenkoSP.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.KurchenkoSP.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.KurchenkoSP.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(15, DataService.Addition(5, 10));
+        }
+
+        [TestMethod]
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        [TestMethod]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        [TestMethod]
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(2, DataService.Division(10, 5));
+        }
+    }
+}
---
+++ Tyuiu.KurchenkoSP.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.KurchenkoSP.Sprint0.Task5.V0.Lib;
+
+Console.WriteLine("A + B = " + DataService.Addition(5, 10));
+
+Console.WriteLine("A - B = " + DataService.Subtraction(10, 5));
+
+Console.WriteLine("A * B = " + DataService.Multiplication(10, 5));
+
+Console.WriteLine("A / B = " + DataService.Division(10, 2));
+
+Console.ReadKey();
